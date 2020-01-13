32eb09f08b1a19ab3f3c9f54b2b2cf2444643de7
dominikabieder <dominika@cultivate.team>
2020-01-13 11:46:44 +0100

    Set up Serverless infrastructure

---
+++ list.js
@@ -0,0 +1,19 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export const main = async event => {
+    const params = {
+        TableName: process.env.tableName,
+        KeyConditionExpression: "userId = :userId", //only returns items with matching userId partition key
+        ExpressionAttributeValues: {
+            ":userId": event.requestContext.identity.cognitoIdentityId //defines the value in the condition :userId
+        }
+    };
+
+    try {
+        const result = await dynamoDbLib.call("query", params);
+        return success(result.Items);
+    } catch (e) {
+        return failure({ status: false });
+    }
+};
